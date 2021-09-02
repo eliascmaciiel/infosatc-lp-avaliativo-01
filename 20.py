5dab8e0223877d70451abbbd300dc555f2e09128
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-02 13:48:44 -0300

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,6 @@
+print("Olá, seja muito bem vindo, peço que você digite um número em quilogramas e iremos lhe apresentar convertido em libras.")
+
+K = float(input("Valor em kg: "))
+L =  K/0.45
+
+print ("{}kg, convertido em libras é: {}".format(K,L))
