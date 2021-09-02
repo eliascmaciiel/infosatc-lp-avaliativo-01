445f89530d57799645f0b625c56f6a1b3176cdef
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-02 13:41:29 -0300

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,7 @@
+print("Olá, seja muito bem vindo, peço que você digite uma velocidade em kilômetros por hora e lhe iremos apresentar em milhas por hora")
+
+km = float(input("Digite uma velocidade em km/h: "))
+
+milhas = km / 1.61
+
+print ("A velocidade convertida em mph é: {}".format(milhas))
