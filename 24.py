9fd3f92f0ff33815461789f9cceb378e83d112aa
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-02 13:52:15 -0300

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,6 @@
+print("Olá, seja muito bem vindo, peço que você digite um comprimento em m² e iremos lhe apresentar convertido em acres.")
+
+M = float(input("Número em m²: "))
+A =  M*0.000247
+
+print ("{} m², convertido em acres é: {}.".format(M,A) )
