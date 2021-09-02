b99e9ac15b3f4087014a737063a97e60adfae899
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-02 13:46:02 -0300

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,5 @@
+print("Olá, seja muito bem vindo, peço que você digite um número em m³ e iremos lhe apresentar em litros.")
+
+M = float(input("Numero em m³: "))
+L =  1000*M
+print ("{} m³ em litros é: {}:".format(M,L))
