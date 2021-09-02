e212e9855c6c94f269672d1eddba726d5649278c
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-02 13:55:46 -0300

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,6 @@
+print("Olá, seja muito bem vindo, peço que você digite um comprimento em hectares e iremos lhe apresentar convertido em m².")
+
+H = float(input("Número em hectares: "))
+M =  H * 10000
+
+print ("{} hectares, convertido em m² é: {}".format(H,M) )
