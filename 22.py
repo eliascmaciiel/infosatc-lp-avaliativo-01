3d2b03280debc558c9c3226ca3954afed49c2997
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-02 13:57:20 -0300

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,6 +1,6 @@
 print("Olá, seja muito bem vindo, peço que você digite um comprimento em jardas e iremos lhe apresentar convertido em metros.")
 
 J = float(input("Número em Jardas: "))
-M =  0.91*J
+M =  0.91 * J
 
 print ("{} Jardas, convertido em metros é: {} metros.".format(J,M))
