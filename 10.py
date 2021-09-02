af7662e76b61bd4b9cd3361825cf43b14487e113
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-02 13:41:09 -0300

    10

---
+++ 10.py
@@ -0,0 +1,7 @@
+print("Olá, seja muito bem vindo, peço que você digite uma velocidade em km/h e lhe iremos apresentar em m/s")
+
+km = float(input("Digite uma velocidade em km/h: "))
+
+metros = km/3.6
+
+print ("A velocidade convertida em m/s é: {}".format(metros))
